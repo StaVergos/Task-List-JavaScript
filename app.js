77c0717dbcbdea2ab73edb8d273f0cebb4ec9954
Stavros Vergos <stavrospvergos@gmail.com>
2020-03-31 15:37:10 +0300

    initial commit

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -20,27 +26,59 @@ function addTask(e) {
     alert('Add a task');
   }
 
-  //Create li element
+  // Create li element
   const li = document.createElement('li');
-  //Add class
+  // Add class
   li.className = 'collection-item';
-  //Create text node and append to li
+  // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
-  //Add class
+  // Add class
   link.className = 'delete-item secondary-content';
-  //Add icon html
+  // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
-  il.appendChild(link);
-  //Append li to ul
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
   taskList.appendChild(li);
 
-  //Clear input
+  // Clear input
   taskInput.value = '';
-  
-  console.log(li);
 
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')) {
+    e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
